de8787c5f132ebd79d71d587cd61e85d37f7b5f0
Pedro Barros Vivas <138531144+Pedrobvivas@users.noreply.github.com>
2024-04-09 09:52:38 -0300

    Add files via upload

---
+++ analisador_sentimento.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+def carrega(nome_do_arquivo):
+    try:
+        with open(nome_do_arquivo, "r") as arquivo:
+            dados = arquivo.read()
+            return dados
+    except IOError as e:
+        print(f"Erro: {e}")
+        
+def salva(nome_do_arquivo, conteudo):
+    try:
+        with open(nome_do_arquivo, "w", encoding="utf-8") as arquivo:
+            arquivo.write(conteudo)
+    except IOError as e:
+        print(f"Erro ao salvar arquivo: {e}")
+        
+def analisador_sentimentos(produto):
+    prompt_sistema = f"""
+        Você é um analisador de sentimentos de avaliações de produtos.
+        Escreva um parágrafo com até 50 palavras resumindo as avaliações e 
+        depois atribua qual o sentimento geral para o produto.
+        Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+
+        # Formato de Saída
+
+        Nome do Produto:
+        Resumo das Avaliações:
+        Sentimento Geral: [utilize aqui apenas Positivo, Negativo ou Neutro]
+        Ponto fortes: lista com três bullets
+        Pontos fracos: lista com três bullets
+    """
